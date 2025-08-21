7cb6e7b4c55c6a9de6c8ddee4a02966643442281
praveen <praveenm@motorola.com>
2025-08-21 15:43:58 +0530

    final modification

---
+++ second.py
@@ -0,0 +1,9 @@
+def add(x, y):
+  return x + y
+
+if __name__ == "__main__":
+  x = int(input("enter the number for x value: "))
+  y = int(input("enter the number for y value: "))
+  z = add(x, y)
+  print("the result of adding two number:", z)
+
